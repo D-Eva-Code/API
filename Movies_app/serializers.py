5dd1c5b43ed3e0a872c87b294896bcab0b6e5733
Eva <anagwonyeadaeze42@gmail.com>
2024-12-31 15:02:05 +0100

    Movie Api

---
+++ Movies_app/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Movie
+
+class Movieserializers(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'name', 'duration', 'rating']
